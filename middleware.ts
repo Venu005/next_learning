ceff225f58ef5295184e21c774a00286218706d2
Venu005 <venusaiyalamanchili@gmail.com>
2024-04-30 13:45:04 +0530

    fix bugs in middleware to redirect to dashboard after signin

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,10 @@ export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXT_AUTH_SECRET,
+  });
   const url = request.nextUrl;
   if (
     token &&
