b40b412570f826c007b0f1cce8b27a1b33decf84
Venu005 <venusaiyalamanchili@gmail.com>
2024-04-30 20:15:12 +0530

    deleteMessages api complete testing tbd

---
+++ app/api/deleteMessages/[messageid]/route.ts
@@ -0,0 +1,62 @@
+import { dbconnect } from "@/lib/db";
+import { User } from "next-auth";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import UserModel from "@/models/user";
+
+export default async function DELETE(
+  req: Request,
+  { params }: { params: { messageid: string } }
+) {
+  const messageId = params.messageid;
+  await dbconnect();
+  try {
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user;
+    if (!session || !user) {
+      return Response.json(
+        {
+          success: false,
+          message: "Not authenticated",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+    const updatedResult = await UserModel.updateOne(
+      { _id: user._id },
+      {
+        $pull: {
+          messages: { _id: messageId },
+        },
+      }
+    );
+    if (updatedResult.modifiedCount == 0) {
+      return Response.json({
+        success: false,
+        message: "Message already deleted or doesn't exist",
+      });
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "Message deleted successfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error: any) {
+    console.log("Error in deleting a message", error?.message);
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to delete message",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
